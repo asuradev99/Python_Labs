7b5799cef97204267a8ac42754cd9a9781a3dacf
Aditya Suresh <ethanaditya@gmail.com>
2019-08-03 16:24:07 -0600

    Finished Peer-to-Peer project. Server and client are fully commented and cleaned, completely scalable project.

--- Peer-To-PeerProject/Peer_1/Main_1.py
+++ Peer-To-PeerProject/Peer_1/Main_1.py
@@ -4,101 +4,150 @@ import time
 import threading
 #Directory of target file 
 file_dir = "File_1.txt" 
+
 #Initialize dict of target files and their corresponding ports 
 peer_table = {}
 
 #number of peers
-peerCount = 2
+peerCount = 3
+
 #Socket initialization
 server_port = 3001
 l_addr = '127.0.0.1'
-cn_sock = socket(AF_INET, SOCK_STREAM)
-cn_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-cn_sock.bind((l_addr, server_port))
+s_sock = socket(AF_INET, SOCK_STREAM)
+s_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+s_sock.bind((l_addr, server_port))
 
-c_sock = socket (AF_INET, SOCK_STREAM)
+#Client socket initialization
+cn_sock = socket (AF_INET, SOCK_STREAM)
+
 #Server Hander (multithreaded)
 class ServerHandler(threading.Thread):
+    #initialize server handler 
     def __init__(self, connection_socket, _id):        
         threading.Thread.__init__(self)
         self.connection_socket = connection_socket
         self._id = _id
         
-
+    #server body code
     def run(self):
         #Add file to dict 
         peer_table ["File_1.txt"] = '3001' 
+
+        #format peer table into string for transportation 
         def format_peer_table():
             k = []
             for key, value in peer_table.items():
                 k.append(key)
                 k.append(value)
             return ','.join(k)
+        
+        #recieve file data from client and append it to local peer table
         def set_peer_table(self):
-                #Assuming the client socket is already connected parse recieved data into dict pair
+                print ("Thread {} accepted ADD FILE request".format(self._id))
+                #recieve file data from client 
                 rcvd_port, rcvd_filename = self.client_socket.recv(48).decode().split(',')
+                #add to peer table
                 peer_table[rcvd_filename] = rcvd_port
-                print (peer_table)
+                print ("Updating peer table: " + str(peer_table))
+
+        #when required send peer table over socket to client 
         def give_peer_table(self):
+                #make sure peer table is complete before sending 
                 if(len(peer_table) == peerCount):
+                    #send request accepted message to client 
                     self.client_socket.send("REQ ACPT".encode())
                     print ("Thread {} accepted REQ DICT request".format(self._id))
+                    #send formatted peer table (string) to client 
                     self.client_socket.send(format_peer_table().encode())
                 else:
+                    #send request denied message to client if peer table incomplete 
                     self.client_socket.send("REQ DENY".encode())
-                    print ("Thread {} denied REQ DICT request".format(self._id))
+                    print ("Thread {} denied REQ DICT request: {}".format(self._id, peer_table))
+
+        #when local file requested, send it to client 
         def send_file(self, _file):
+                print ("Thread {} accepted REQ FILE request".format(self._id))
+                #open local file
                 with open (_file) as f:
+                    #copy file to string and send string 
                     otpt_f = f.read()
                     self.client_socket.send(otpt_f.encode())
                     print ("Thread {} sent {}".format(self._id, file_dir))
+
+        #main loop: listening for connection, accept connection, take action, close connection repeat
         while True:
-            print ("Thread " + str(self._id) + ' ' + "initialized and listening" )
+            #begin listening for connections 
             self.connection_socket.listen(1)
+            print ("Thread " + str(self._id) + ' ' + "initialized and listening..." )
+            #accept connection 
             self.client_socket, self.addr = self.connection_socket.accept()
             print ("Thread {} connected to ".format(self._id) + str(self.addr))
+            #recieve an 8-byte control message to take specific actions
             self.ctrl = self.client_socket.recv(8).decode()
-            
-            #Control messages
+
+            #Control messages:
+
+            #Client wants to update local peer table
             if self.ctrl == 'ADD FILE':
                 print ("Thread {} accepted ADD FILE request".format(self._id))
                 set_peer_table(self)
+            #client requests local peer table
             elif self.ctrl == 'REQ DICT':
                 give_peer_table(self)
+            #client requests local file 
             elif self.ctrl == 'REQ FILE':
                 send_file(self, file_dir)
-            time.sleep(1)
+            #wait to reset socket so client can close properly 
+            time.sleep(0.01)
             print ("Thread {} resetting socket".format(self._id))
+            #close connection and repeat
             self.client_socket.close()
+
+#Client handler (to request files) 
 class ClientHandler ():
+    #initialize client handler 
     def __init__(self, client_socket, addr):
         #initialize socket 
         self.client_socket = client_socket
         self.addr = addr
+    
+    #method to obtain a file as instructed from the local peer table
     def obtain_file_data (self, filename, port ):
+        #initialize socket 
         self.client_socket = socket(AF_INET, SOCK_STREAM)
         self.client_socket.connect ((self.addr, port))
+        #send a request for file control message 
         self.client_socket.send("REQ FILE".encode())
         print ("retrieving " + filename + " from server")
+        #open file and write contents of recieved file data to it 
         with open (filename, 'x') as f:
             f.write(self.client_socket.recv(512).decode())
+            #close connection 
             f.close()
+
+    #loops which iterates through local peer table and obtains each file via obtain_file_data()
     def begin_all_file_req(self, table):
-        print ("waiting ")
+        print ("Client waiting for peers to finish sending their files and download the peer table...")
+        #wait for peer table to be complete
         while len(peer_table) < peerCount: 
            pass
+        #iterate through the local peer table and obtain each file reference in it 
+        print ("Client ready to begin requesting")
         for key, value in table.items():
-            print ("initiating contact")
+            #make sure the file reference isn't to the local file 
             if key != file_dir:
+                print ("Requesting " + key + " from port " + value)
+                #run obtain_file_data with key, the name of the file, and value, the port 
                 self.obtain_file_data(key, int(value))
         
 #main body 
 
+#create server threads 
 for i in range (peerCount * 3):
-    server_thread = ServerHandler(cn_sock, i +1)
+    server_thread = ServerHandler(s_sock, i +1)
     server_thread.start()
-
-client_main = ClientHandler(c_sock, l_addr)
-time.sleep(4)
-print (peer_table)
+#create client handler
+client_main = ClientHandler(cn_sock, l_addr)
+#start requesting files from peers
 client_main.begin_all_file_req(peer_table)
--- Peer-To-PeerProject/Peer_3/Main_3.py
+++ Peer-To-PeerProject/Peer_3/Main_3.py
@@ -1,8 +1,11 @@
+#Peer 3
 from socket import *
+import time
 import threading
 
 #Directory of target file 
-file_dir = "/Peer-To-PeerProject/Peer_3/File_3.txt"
+file_dir = "File_3.txt"
+
 #local peer table
 l_peer_table = {}
 
@@ -10,17 +13,47 @@ l_peer_table = {}
 server_port = 3003
 l_addr = '127.0.0.1'  
 cn_sock = socket(AF_INET, SOCK_STREAM)
+
 #Server socket initialization
 s_sock = socket(AF_INET, SOCK_STREAM)
 s_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 s_sock.bind((l_addr, server_port))
 
+#Server Handler (multithreaded)
 class ServerHandler(threading.Thread):
-    def __init__(self, client_socket, addr):
-        self.client_socket = client_socket
-        self.addr = addr
+    #initialize server handler 
+    def __init__(self, connection_socket, _id):
+        threading.Thread.__init__(self)
+        self.connection_socket = connection_socket
+        self._id = _id
+
+    #server body code
     def run(self):
-        pass
+        #when local file requested, send it to client 
+        def send_file(self, _file):
+            #open local file
+            with open (_file) as f:
+                #copy file to string and send string 
+                otpt_f = f.read()
+                self.client_socket.send(otpt_f.encode())
+                print ("Thread {} sent {}".format(self._id, file_dir))
+        #main loop: listening for connection, accept connection, take action, close connection repeat
+        while True:
+            #begin listening for connections 
+            self.connection_socket.listen(1)
+            print ("Thread " + str(self._id) + ' ' + "initialized and listening" )
+            #accept connection 
+            self.client_socket, self.addr = self.connection_socket.accept()
+            print ("Thread {} connected to ".format(self._id) + str(self.addr))
+            #recieve an 8-byte control message to take specific actions
+            self.ctrl = self.client_socket.recv(8).decode()
+            #Request file message 
+            if self.ctrl == 'REQ FILE':
+                send_file(self, file_dir)
+            
+            time.sleep(0.01)
+            print ("Thread {} resetting socket".format(self._id))
+            self.client_socket.close()
 
 class ClientHandler ():
     def __init__(self, client_socket, addr):
@@ -28,15 +61,77 @@ class ClientHandler ():
         self.client_socket = client_socket
         self.addr = addr
     def inform_server(self):
-        #process to give file name and port to server so other peers can access it
+        print ("Connecting to server to add file to server peer table...")
         #connect to server socket 
         self.client_socket.connect ((self.addr, 3001))
+        self.client_socket.send("ADD FILE".encode())
         #send file name and port to server 
         self.client_socket.sendall((str(server_port) + "," + file_dir).encode())
         print ("Peer 3 has delivered it's file to the server")
-        #close socket
-       
-        
+        #close socket 
+        self.client_socket.shutdown(SHUT_RDWR)
+        self.client_socket.close()
 
+    def obtain_peer_table(self, delay):
+        self.rcvd_ctrl = ""
+        #loop to repeat requesting peer table until it is complete 
+        while self.rcvd_ctrl != "REQ ACPT":
+            #initialize client socket 
+            self.client_socket = socket(AF_INET, SOCK_STREAM)
+            self.client_socket.connect ((self.addr, 3001))
+            #send request peer table control message 
+            self.client_socket.send ("REQ DICT".encode())
+            #recieve 8-byte control message response 
+            self.rcvd_ctrl = self.client_socket.recv(8).decode()
+            #break if recieved request accepted message 
+            if self.rcvd_ctrl == "REQ ACPT": break
+            #close connection 
+            self.client_socket.shutdown(SHUT_RDWR)
+            self.client_socket.close()
+            print ("REQ DICT denied, waiting {} seconds: {}".format(delay, self.rcvd_ctrl))
+            #wait a delay period until retrying
+            time.sleep(delay)
+            delay *= 2 
+        print ("REQ DICT accepted, recieving peer dict")
+        #parse recieved string and format it into dict 
+        table = self.client_socket.recv(256).decode().split(',')
+        table = dict(zip(*[iter(table)]*2))
+        #close socket 
+        self.client_socket.shutdown(SHUT_RDWR)
+        self.client_socket.close()
+        #return formatted dict 
+        return table
+    def obtain_file_data (self, filename, port ):
+        #initialize socket 
+        self.client_socket = socket(AF_INET, SOCK_STREAM)
+        self.client_socket.connect ((self.addr, port))
+        #send a request for file control message 
+        self.client_socket.send("REQ FILE".encode())
+        print ("retrieving " + filename + " from server")
+        #open file and write contents of recieved file data to it 
+        with open (filename, 'x') as f:
+            f.write(self.client_socket.recv(512).decode())
+            #close connection
+            f.close()
+
+    def begin_all_file_req(self, table):
+        print ("Client waiting for peers to finish sending their files and download the peer table...")
+        #wait for peer table to be complete
+        for key, value in table.items():
+            #make sure the file reference isn't to the local file 
+            if key != file_dir:
+                print ("Requesting " + key + " from port " + value)
+                #run obtain_file_data with key, the name of the file, and value, the port 
+                self.obtain_file_data(key, int(value))
+
+#create server threads             
+for i in range (3):
+    server_thread = ServerHandler(s_sock, i+1)
+    server_thread.start()
+
+#create client handler
 client_main = ClientHandler(cn_sock, l_addr)
 client_main.inform_server()
+l_peer_table = client_main.obtain_peer_table(0.1)
+client_main.begin_all_file_req(l_peer_table)
+print ("Done")
